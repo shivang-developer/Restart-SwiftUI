a3e31c056462d25b176ee7b69ad87175c63fcea0
Shivang Mishra <shivangm@exzeo.com>
2023-06-08 15:56:17 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Shivang Mishra on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
